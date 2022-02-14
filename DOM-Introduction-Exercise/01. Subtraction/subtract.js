8160da7635f3f7a53c89ae17c44f645d8d4f854a
kokiqn <kaloyan.stefanoff@gmail.com>
2022-02-14 18:40:20 +0200

    add solution to problem

--- DOM-Introduction-Exercise/01. Subtraction/subtract.js
+++ DOM-Introduction-Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    // TODO
+    let firstNumber = Number(document.getElementById('firstNumber').value);
+    let secondNumber = Number(document.getElementById('secondNumber').value);
+    let resultDiv = document.getElementById('result');
+    resultDiv.textContent = firstNumber - secondNumber;
 }
