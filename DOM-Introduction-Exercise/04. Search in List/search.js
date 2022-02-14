9b73cb646c552e7fe7b9f975a8f5370b01e63678
kokiqn <kaloyan.stefanoff@gmail.com>
2022-02-14 20:21:28 +0200

    add solution to problem

--- DOM-Introduction-Exercise/04. Search in List/search.js
+++ DOM-Introduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,13 @@
 function search() {
-   // TODO
+   let townsListElements = Array.from(document.querySelectorAll('#towns li'));
+   let searchValue = document.getElementById('searchText').value;
+   let matchesFound = townsListElements.filter(item => item.textContent.includes(searchValue));
+
+   matchesFound.forEach(match => {
+      match.style.fontWeight = 'bold';
+      match.style.textDecoration = 'underline';
+   });
+
+   let result = document.getElementById('result');
+   result.textContent = `${matchesFound.length} matches found`;
 }
