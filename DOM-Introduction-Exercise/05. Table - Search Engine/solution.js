f5ff92ac76be686eacd391acba14b4a4a92b6fc6
kokiqn <kaloyan.stefanoff@gmail.com>
2022-02-14 20:39:14 +0200

    add solution to problem

--- DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,13 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let tableRowElements = Array.from(document.querySelectorAll('.container tbody tr'));
+      tableRowElements.forEach(el => el.classList.remove('select'));
+      let searchField = document.getElementById('searchField');
 
+      let matchesFound = tableRowElements.filter(el => el.textContent.includes(searchField.value));
+      matchesFound.forEach(match => match.classList.add('select'));
+
+      searchField.value = '';
    }
 }
