99ade8a34d9c37ac24b55fc4bf5c14f9ee3d2cb2
kokiqn <kaloyan.stefanoff@gmail.com>
2022-02-15 17:56:26 +0200

    add solution to problem

--- DOM-Introduction-Exercise/06. Format the Text/solution.js
+++ DOM-Introduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,11 @@
 function solve() {
-  //TODO
+  let textAreaElement = document.getElementById('input');
+  let outputDiv = document.getElementById('output');
+  let sentences = textAreaElement.value.split(/\s*\.\s*/).filter(Boolean);
+
+  while (sentences.length > 0) {
+    let current = sentences.splice(0, 3);
+    let paragraph = current.join('. ') + '.';
+    outputDiv.innerHTML += `<p>${paragraph}</p>`
+  }
 }
