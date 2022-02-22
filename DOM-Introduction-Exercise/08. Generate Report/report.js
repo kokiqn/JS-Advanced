e3541e8c3cfea8570aaf6dc16a3b813cb79d48e9
kokiqn <kaloyan.stefanoff@gmail.com>
2022-02-22 18:51:19 +0200

    add solution to problem

--- DOM-Introduction-Exercise/08. Generate Report/report.js
+++ DOM-Introduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,15 @@
 function generateReport() {
-    //TODO
+    let tableHeaders = document.querySelectorAll('input');
+    let tableRows = Array.from(document.querySelectorAll('tbody > tr')).map(x => Array.from(x.children));
+    let outputData = []
+
+    for (let tr = 0; tr < tableRows.length; tr++) {
+        let obj = {};
+        for (let td = 0; td < tableRows[tr].length; td++) {
+            if (tableHeaders[td].checked) obj[tableHeaders[td].name] = tableRows[tr][td].textContent;
+        }
+        outputData.push(obj);
+    }
+
+    document.getElementById('output').value = JSON.stringify(outputData);
 }
