7a612b75f6e2584e1205166fa963633ce78e648e
kokiqn <kaloyan.stefanoff@gmail.com>
2022-02-23 15:17:25 +0200

    add solution to problem

--- DOM-Introduction-Exercise/09. Number Convertor/solution.js
+++ DOM-Introduction-Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,15 @@
 function solve() {
+    let inputElement = document.getElementById('input');
+    let outputElement = document.getElementById('result');
 
-    //TODO...
+    let dropdownElement = document.getElementById('selectMenuTo');
+    let buttonElement = document.querySelector('button');
+
+    dropdownElement.innerHTML = '<option selected value=binary>Binary</option>'
+        + '<option value=hexadecimal>Hexadecimal</option>';
+
+    buttonElement.addEventListener('click', () => {
+        if (dropdownElement.value === 'binary') outputElement.value = Number(inputElement.value).toString(2);
+        else outputElement.value = Number(inputElement.value).toString(16).toUpperCase();
+    })
 }
