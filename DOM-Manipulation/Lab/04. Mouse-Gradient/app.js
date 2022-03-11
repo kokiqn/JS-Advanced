767b2836e1a8e83d029d32f7b13e93b7e32d38f0
kokiqn <kaloyan.stefanoff@gmail.com>
2022-03-11 19:39:05 +0200

    add solution to problem

--- DOM-Manipulation/Lab/04. Mouse-Gradient/app.js
+++ DOM-Manipulation/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,12 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', mouseMoveHandler);
+    gradientElement.addEventListener('mouseout', () => resultElement.textContent = '');
+
+    function mouseMoveHandler(e) {
+        let percent = Math.trunc((e.offsetX / (e.target.clientWidth - 1) * 100));
+        resultElement.textContent = `${percent}%`;
+    }
 }
