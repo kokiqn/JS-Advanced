59d40bd02e651e954fff0aa3795f32a2f14720c0
kokiqn <kaloyan.stefanoff@gmail.com>
2022-03-11 18:33:39 +0200

    add solution to problem

--- DOM-Manipulation/Lab/03. Add-Delete/app.js
+++ DOM-Manipulation/Lab/03. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+    let newItemText = document.getElementById('newItemText');
+    let itemListElement = document.getElementById('items');
+
+    let liItemElement = document.createElement('li');
+    liItemElement.textContent = newItemText.value;
+
+    // Add delete button
+    let deleteButtonElement = document.createElement('a');
+    deleteButtonElement.setAttribute('href', '#');
+    deleteButtonElement.textContent = '[Delete]';
+
+    // Attach event to button
+    deleteButtonElement.addEventListener('click', e => e.currentTarget.parentNode.remove())
+
+    // Append to HTML code
+    liItemElement.appendChild(deleteButtonElement);
+    itemListElement.appendChild(liItemElement);
+    newItemText.value = '';
 }
