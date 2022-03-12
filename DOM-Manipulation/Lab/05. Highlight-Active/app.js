861eceae5a3ef87276315dc5a763d72bd135ca12
kokiqn <kaloyan.stefanoff@gmail.com>
2022-03-12 13:16:22 +0200

    add solution to problem

--- DOM-Manipulation/Lab/05. Highlight-Active/app.js
+++ DOM-Manipulation/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    let inputFields = document.querySelectorAll('input');
+    for (const inputField of inputFields) {
+        inputField.addEventListener('focus', onFocus);
+        inputField.addEventListener('blur', onBlur);
+    }
+
+    function onFocus(e) {
+        let parentDiv = e.target.parentNode;
+        parentDiv.classList.add('focused');
+    }
+
+    function onBlur(e) {
+        let parentDiv = e.target.parentNode;
+        parentDiv.classList.remove('focused');
+    }
 }
