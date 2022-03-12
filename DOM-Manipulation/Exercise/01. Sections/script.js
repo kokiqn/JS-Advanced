6d387e11055b2bbbd95615eab4aca050d65285cd
kokiqn <kaloyan.stefanoff@gmail.com>
2022-03-12 15:42:15 +0200

    add solution to problem

--- DOM-Manipulation/Exercise/01. Sections/script.js
+++ DOM-Manipulation/Exercise/01. Sections/script.js
@@ -1,3 +1,14 @@
 function create(words) {
-   console.log('TODO:...');
+   words.forEach(str => {
+      let paragraph = document.createElement('p');
+      let div = document.createElement('div');
+
+      paragraph.textContent = str;
+      paragraph.style.display = 'none';
+      div.appendChild(paragraph);
+      div.addEventListener('click', () => paragraph.style.display = 'block');
+
+      let contentElement = document.getElementById('content');
+      contentElement.appendChild(div);
+   })
 }
