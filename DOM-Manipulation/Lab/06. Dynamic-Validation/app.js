8016286c14619605457aee8b21e0ef143ecc07ca
kokiqn <kaloyan.stefanoff@gmail.com>
2022-03-12 14:13:39 +0200

    add solution to problem

--- DOM-Manipulation/Lab/06. Dynamic-Validation/app.js
+++ DOM-Manipulation/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    let inputFieldElement = document.getElementById('email');
+    let validEmailRegex = /\b([a-z]+)@([a-z]+)\.([a-z]+)/g
+
+    inputFieldElement.addEventListener('change', (e) => {
+        if (!e.target.value.match(validEmailRegex)) {
+            inputFieldElement.classList.add('error');
+        } else {
+            inputFieldElement.classList.remove('error');
+            inputFieldElement.value = '';
+        }
+    })
 }
