60df9d0221b72d49b466c13aaec36c115b16f413
kokiqn <kaloyan.stefanoff@gmail.com>
2022-03-13 11:18:39 +0200

    add solution to problem

--- DOM-Manipulation/Exercise/03. Locked-Profile/app.js
+++ DOM-Manipulation/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let showMoreButtons = [...document.querySelectorAll('button')];
+    showMoreButtons.forEach(btn => btn.addEventListener('click', toggleFields));
+
+    function toggleFields(e) {
+        let button = e.target;
+        let currentProfile = button.parentElement;
+        let isLocked = currentProfile.querySelector('input[type="radio"]').checked;
+        let hiddenFields = currentProfile.querySelector('div');
+
+        if (isLocked) return;
+
+        if (!hiddenFields.style.display) {
+            hiddenFields.style.display = 'block';
+            button.textContent = 'Hide it';
+        } else {
+            hiddenFields.style.display = '';
+            button.textContent = 'Show more';
+        }
+    }
 }
