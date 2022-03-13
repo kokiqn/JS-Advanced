43c2f49468a5b19d4b753771c940bedbbef2728b
kokiqn <kaloyan.stefanoff@gmail.com>
2022-03-13 11:42:46 +0200

    add solution to problem

--- DOM-Manipulation/Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM-Manipulation/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+
+    if (!newItemText.value && !newItemValue.value) return;
+
+    let newOption = document.createElement('option');
+    newOption.text = newItemText.value;
+    newOption.value = newItemValue.value;
+    document.getElementById('menu').appendChild(newOption);
+
+    newItemText.value = '';
+    newItemValue.value = '';
 }
