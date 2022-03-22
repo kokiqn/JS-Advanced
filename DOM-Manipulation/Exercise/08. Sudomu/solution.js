314ed9ede08c5e879eb9762ed02dad42e87eaf87
kokiqn <kaloyan.stefanoff@gmail.com>
2022-03-22 11:41:56 +0200

    add solution to problem

--- DOM-Manipulation/Exercise/08. Sudomu/solution.js
+++ DOM-Manipulation/Exercise/08. Sudomu/solution.js
@@ -1,4 +1,61 @@
 function solve() {
+    let tableRows = Array.from(document.querySelectorAll('tbody > tr'));
+    let tableElement = document.querySelector('table');
+    let inputElements = document.querySelectorAll('td > input');
+    let paragraphElement = document.querySelector('#check > p');
+    let [checkBtn, clearBtn] = document.querySelectorAll('button');
 
-    //TODO...
+    checkBtn.addEventListener('click', onQuickCheck);
+    clearBtn.addEventListener('click', onClear);
+
+    function onQuickCheck() {
+        let isSolved = checkMatrix();
+        tableElement.style.border = isSolved ? '2px solid green' : '2px solid red';
+        paragraphElement.style.color = isSolved ? 'green' : 'red';
+        paragraphElement.textContent = isSolved ? 'You solve it! Congratulations!' : 'NOP! You are not done yet...';
+    }
+
+    function onClear() {
+        inputElements.forEach(v => v.value = '');
+        tableElement.style.border = '';
+        paragraphElement.textContent = '';
+    }
+
+    function createMatrix() {
+        let matrix = [];
+        tableRows.forEach(tr => {
+            let tds = Array.from(tr.children);
+            let tdArr = tds.map(td => td.children[0].value);
+            matrix.push(tdArr);
+        });
+
+        return matrix;
+    }
+
+    function checkMatrix() {
+        let matrix = createMatrix();
+        // let isWin = true; replaced by a direct return false/true
+        for (let row = 0; row < matrix.length; row++) {
+            let rowsToCheck = {
+                0: [1, 2],
+                1: [0, 2],
+                2: [0, 1]
+            }
+
+            let rowsToCheckValues = rowsToCheck[row];
+            let currentRow = matrix[row];
+
+            for (let col = 0; col < matrix[row].length; col++) {
+                let currentValue = matrix[row][col];
+
+                for (const r of rowsToCheckValues) {
+                    if (matrix[r][col] == currentValue) return false;
+                }
+
+                if (currentRow.lastIndexOf(currentValue) != col) return false;
+            }
+        }
+
+        return true;
+    }
 }
--- DOM-Manipulation/Exercise/09. JavaScript-Quiz/solution.js
+++ DOM-Manipulation/Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+    let quizzieDiv = document.getElementById('quizzie');
+    let correctAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'];
+    let questionSections = Array.from(document.querySelectorAll('section'));
+    let rightAnswers = 0;
+
+    quizzieDiv.addEventListener('click', onClick);
+
+    function onClick(e) {
+        let answerText = e.target;
+        if (answerText.classList[0] != 'answer-text') return;
+        if (correctAnswers.includes(answerText.textContent)) rightAnswers++;
+
+        questionSections.shift().style.display = 'none';
+        if (questionSections.length) {
+            questionSections[0].style.display = 'block';
+        } else {
+            document.getElementById('results').style.display = 'block';
+            document.querySelector('li > h1').textContent = rightAnswers == 3 ? 'You are recognized as top JavaScript fan!' : `You have ${rightAnswers} right answers`;
+        }
+    }
 }
