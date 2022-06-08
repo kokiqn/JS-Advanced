df38b6952b195ee11b3035c88e763100f7fe42e9
kokiqn <kaloyan.stefanoff@gmail.com>
2022-06-08 17:01:28 +0300

    add solution to problem

--- Advanced-Functions/Exercise/08. Next Article/script.js
+++ Advanced-Functions/Exercise/08. Next Article/script.js
@@ -1,3 +1,10 @@
 function getArticleGenerator(articles) {
-    // TODO
+    return function () {
+        if (articles.length) {
+            let contentDiv = document.getElementById('content');
+            let articleElement = document.createElement('article');
+            articleElement.textContent = articles.shift();
+            contentDiv.appendChild(articleElement);
+        }
+    }
 }
