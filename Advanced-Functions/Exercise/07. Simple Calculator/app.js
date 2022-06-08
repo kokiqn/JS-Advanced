900e60572c78ae0266b8f311958fd9c7d7743f59
kokiqn <kaloyan.stefanoff@gmail.com>
2022-06-08 16:48:16 +0300

    add solution to problem

--- Advanced-Functions/Exercise/07. Simple Calculator/app.js
+++ Advanced-Functions/Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,20 @@
 function calculator() {
-    // TODO:
+    let firstNum;
+    let secondNum;
+    let result;
+
+    return {
+        init(selector1, selector2, resultSelector) {
+            firstNum = document.querySelector(selector1);
+            secondNum = document.querySelector(selector2);
+            result = document.querySelector(resultSelector);
+        },
+        add() {
+            result.value = Number(firstNum.value) + Number(secondNum.value);
+        },
+        subtract() {
+            result.value = Number(firstNum.value) - Number(secondNum.value);
+        }
+
+    }
 }
-
-
-
-
