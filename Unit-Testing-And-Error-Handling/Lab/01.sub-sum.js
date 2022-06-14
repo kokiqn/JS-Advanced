dce415af709a873f88b06edb5d56315c025bb519
kokiqn <kaloyan.stefanoff@gmail.com>
2022-06-14 12:23:10 +0300

    add solution to problem

---
+++ Unit-Testing-And-Error-Handling/Lab/01.sub-sum.js
@@ -0,0 +1,16 @@
+function solve(numbers, start, end) {
+    if (typeof (numbers) != typeof ([])) return NaN;
+    if (start < 0) start = 0;
+    if (end >= numbers.length) end = numbers.length - 1;
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += Number(numbers[i]);
+    }
+    return sum;
+}
+
+console.log(solve([10, 20, 30, 40, 50, 60], -9, 300));
+console.log(solve([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
+console.log(solve([10, 'twenty', 30, 40], 0, 2));
+console.log(solve([], 1, 2));
+console.log(solve('text', 0, 2));
