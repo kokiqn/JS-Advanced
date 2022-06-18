d640a8d0eb0c8e58e91b6aa42368ec5691ee0c39
kokiqn <kaloyan.stefanoff@gmail.com>
2022-06-19 00:59:04 +0300

    add solution to problem

--- Unit-Testing-And-Error-Handling/Exercise/06.Dynamic-Validation/app.js
+++ Unit-Testing-And-Error-Handling/Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    const emailFieldElement = document.getElementById('email');
+    emailFieldElement.addEventListener('change', validateEmail);
+
+    function validateEmail(e) {
+        let regEx = /[a-z]+@[a-z]+\.[a-z]+/;
+        if (regEx.test(e.target.value)) {
+            emailFieldElement.classList.remove('error');
+            e.target.value = '';
+        } else {
+            emailFieldElement.classList.add('error');
+        }
+    }
 }
